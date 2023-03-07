30838e3f84dab05e9fbc7c7fbfb9b66b90fa8cfb
Alex <alexander.s.carney.ug@dartmouth.edu>
2023-03-06 21:26:26 -0500

    Fixed background image, added requirements file

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.markdown(
     f"""
      <style>
      .stApp {{
-         background: url("https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80");
+         background-image: url("https://tradebrains.in/features/wp-content/uploads/2021/07/stock-market-news-trade-brains.jpg");
          background-size: contain;
          background-repeat: no-repeat;
          -webkit-background-size: cover;
@@ -15,8 +15,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown("<h1 style='text-align: center; color: black; font-style: italic;'>ARIMA Studio</h1>",
+
+st.markdown("<h1 style='text-align: center; color: white; font-style: italic;'>ARIMA Studio</h1>",
             unsafe_allow_html=True)
 st.markdown(
-    "<h4 style='text-align: center; color: grey; font-style: italic;'>Built by Alex Carney, Lauren Goyette, Edmund Aduse Poku</h4>",
+    "<h4 style='text-align: center; color: white; font-style: italic;'>Built by Alex Carney, Lauren Goyette, Edmund Aduse Poku</h4>",
     unsafe_allow_html=True)
---
+++ README.md
@@ -0,0 +1,8 @@
+# Arima Studio
+
+A simple web application designed to mimic our workflow for analyzing time series, and using the ARIMA family of prediction models to predict future stock prices
+
+Check it out at the link below:
+
+https://alex-carney-arimastudio-home-vkqegw.streamlit.app/
+
